53a00e650c29f7dbc15837299aec0bbb3df75774
Conrad Chan <conradchan@hotmail.com.au>
2022-10-10 10:54:35 +1100

    optionally read data from env var STARFIT_DATA

--- src/starfit/__init__.py
+++ src/starfit/__init__.py
@@ -1,7 +1,14 @@
+import os
 from pathlib import Path
 
 DATA_DIR = Path(__file__).parent.resolve() / "data"
 del Path
+
+# If user has defined an environment variable for custom data files, use that instead
+user_data_dir = os.getenv("STARFIT_DATA")
+if user_data_dir is not None:
+    DATA_DIR = user_data_dir
+
 
 from .fit import Direct, Double, Single
 from .ga import Ga
