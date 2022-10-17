d4e18ad44c1241bf7f7c7127f1ee82466f62c6c6
dliptai <31463304+dliptai@users.noreply.github.com>
2022-10-17 19:33:17 +1100

    fix typo (#52)

--- src/starfit/__init__.py
+++ src/starfit/__init__.py
@@ -6,7 +6,7 @@ DATA_DIR = Path(__file__).parent.resolve() / "data"
 # If user has defined an environment variable for custom data files, use that instead
 user_data_dir = getenv("STARFIT_DATA")
 if user_data_dir is not None:
-    user_data_dir = Path(user_data_dir).exanduser().resolve()
+    user_data_dir = Path(user_data_dir).expanduser().resolve()
     if user_data_dir.isdir():
         DATA_DIR = user_data_dir
     else:
