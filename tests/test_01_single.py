8713c30cbfe82196fcf7673d49f9a341dc2d5657
Conrad Chan <conradchan@hotmail.com.au>
2022-10-18 10:55:27 +1100

    revert test to correct expected value

--- tests/test_01_single.py
+++ tests/test_01_single.py
@@ -6,7 +6,7 @@ import starfit
 testdata = [
     ("HE1327-2326.dat", 3.292526847047224),  # data format 1
     ("HE0557-4840.dat", 3.811527192751109),  # data format 2
-    ("BD_80_245.dat", 10.12390641724435),  # data format 3
+    ("BD_80_245.dat", 1.12390641724435),  # data format 3
     ("CI-Chondrite-Lodders2019_f4.dat", 16373.047588932503),  # data format 4
     ("CI-Chondrite-Lodders2019_f5.dat", 16372.053997257417),  # data format 5
     ("SDSS-J102915+172927.dat", 0.7040288685922813),  # data format 6
