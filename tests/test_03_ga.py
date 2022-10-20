1d3607a4fb3c7aab502d15279de44675385b5275
Alexander Heger <alexander.heger@monash.edu>
2022-10-21 08:23:25 +1100

    fix ploting, reduce test_03 runtime

--- tests/test_03_ga.py
+++ tests/test_03_ga.py
@@ -1,13 +1,15 @@
+from numpy import pi as PI
+
 import starfit
 
 
 def test():
     """Test a single star fit"""
     TEST_STAR = "HE1327-2326.dat"
-    FITNESS_THRESHOLD = 3.0
+    FITNESS_THRESHOLD = PI
 
     test_result = starfit.Ga(
-        TEST_STAR, silent=True, time_limit=20, sol_size=3
+        TEST_STAR, silent=True, time_limit=1, sol_size=3
     ).sorted_fitness[0]
 
     assert test_result < FITNESS_THRESHOLD
