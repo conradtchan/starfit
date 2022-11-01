ece1362583c4549602e2fc1297f7677b921540c7
Alexander Heger <2sn@users.noreply.github.com>
2022-11-01 14:43:02 +1100

    only include fitness function by providing fitness.pyf (#95)
    This is a crude patch to fix issues of `numpy.setuptools` shortcomings.  As this will be discontinued within the year, maybe not more is needed for now.

--- setup.py
+++ setup.py
@@ -7,22 +7,26 @@ flags = ["-fPIC", "-Ofast", "-g", "-funroll-loops", "-fno-second-underscore", "-
 cwd = os.getcwd()
 starfit_src = os.path.join(cwd, "src/starfit")
 
+
+fit_sources = [
+    os.path.join(starfit_src, "fitness", x)
+    for x in [
+        "fitness.pyf",
+        "type_def.f90",
+        "powell.f90",
+        "norm.f90",
+        "mleqs.f90",
+        "star_data.f90",
+        "abu_data.f90",
+        "solver.f90",
+    ]
+]
+# to skip fitness.pyf if need be
+fit_sources = [x for x in fit_sources if os.path.isfile(x)]
+
 module_solver = Extension(
     "starfit.fitness._solver",
-    sources=(
-        [
-            os.path.join(starfit_src, "fitness", x)
-            for x in [
-                "type_def.f90",
-                "powell.f90",
-                "norm.f90",
-                "mleqs.f90",
-                "star_data.f90",
-                "abu_data.f90",
-                "solver.f90",
-            ]
-        ]
-    ),
+    sources=fit_sources,
     extra_f90_compile_args=flags,
     f2py_options=[
         "--f2cmap",
